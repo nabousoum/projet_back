2213029d266701462eb3f44644afc9893dcc3c46
nabousoum <seynabousoumare98@outlook.fr>
2022-07-29 11:53:08 +0000

    test

--- src/DataProvider/DetailDataProvider.php
+++ src/DataProvider/DetailDataProvider.php
@@ -3,14 +3,11 @@
 
 namespace App\DataProvider;
 
-use App\Entity\Catalogue;
 use App\Repository\MenuRepository;
 use App\Repository\BurgerRepository;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
-use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use App\Entity\Detail;
 use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
-use phpDocumentor\Reflection\Types\This;
 
 final class DetailDataProvider implements ItemDataProviderInterface, RestrictedDataProviderInterface
 {
@@ -29,7 +26,7 @@ final class DetailDataProvider implements ItemDataProviderInterface, RestrictedD
 
     public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): ?Detail
     {
-        dd($id);
-        //return new Detail($id);
+        //dd($id);
+        return new Detail();
     }
 }
