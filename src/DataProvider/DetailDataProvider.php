e1328070a8def04abe174ee377f0567f9d80cc23
nabousoum <seynabousoumare98@outlook.fr>
2022-07-29 12:42:16 +0000

    test

--- src/DataProvider/DetailDataProvider.php
+++ src/DataProvider/DetailDataProvider.php
@@ -35,7 +35,7 @@ final class DetailDataProvider implements ItemDataProviderInterface, RestrictedD
     public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): ?Detail
     {
         $menu = $this->menuRepo->findOneBy(array('id' => $id));
-        dd($menu);
+        dd($id);
         $burger = $this->burgerRepo->findOneBy(array('id' => $id));
        $detail = new Detail();
         if($id == $menu->getId()){
